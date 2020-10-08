304de7632e21d6ea33e80fc4971189b0eeef3333
Dan Tra <dltra@fcps.edu>
2020-10-08 10:11:41 -0400

    Gson and Json String to Object Object to String

--- app/src/main/java/com/example/gsonexample/MainActivity.java
+++ app/src/main/java/com/example/gsonexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gsonexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Gson gson = new GsonBuilder().create();
+        //convert a String to Object
+        SampleData mSampleData = gson.fromJson( "{ itemNumber:1, description: \"Carrots\", quantity:31 }", SampleData.class);
+        //convert Object to String
+        String json = gson.toJson(mSampleData,SampleData.class);
+        System.out.println("Test: "+json);
+    }
+    public class SampleData {
+        public int itemNumber;
+        public String description;
+        public int quantity;
     }
 }
